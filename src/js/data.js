72cfeeb9766bbf205e355d37f2ab10c51e3659f1
nilberth-escobar <nilberrhescobar@mail.com>
2024-02-12 01:57:23 -0600

    styles, update index and template

--- src/js/data.js
+++ src/js/data.js
@@ -1,18 +1,18 @@
 /* const url = 'https://moviesverse1.p.rapidapi.com/get-by-genre?genre=action';
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b8d238b27fmsh972a2c298f17612p12fafejsn325b02608b36',
-		'X-RapidAPI-Host': 'moviesverse1.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'b8d238b27fmsh972a2c298f17612p12fafejsn325b02608b36',
+    'X-RapidAPI-Host': 'moviesverse1.p.rapidapi.com'
+  }
 };
 
 try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
+  const response = await fetch(url, options);
+  const result = await response.json();
+  console.log(result);
 } catch (error) {
-	console.error(error);
+  console.error(error);
 } */
 
 /* function fetchMovies(genre) {
@@ -56,33 +56,32 @@ try {
     });
    */
 
-    async function fetchMovies(genre) {
-        const url = 'https://moviesverse1.p.rapidapi.com/get-by-genre';
-        const apiKey = 'b8d238b27fmsh972a2c298f17612p12fafejsn325b02608b36'; // Replace with your API key
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': apiKey,
-                'X-RapidAPI-Host': 'moviesverse1.p.rapidapi.com',
-            },
-        };
-    
-        try {
-            const response = await fetch(url + `?genre=${genre}`, options);
-            const data = await response.json();
-    
-            // Assuming the API response contains a key like 'movies' that holds the array
-            const moviesArray = data.movies || [];
-            console.log('Fetched movies:', moviesArray);
-    
-            if (!Array.isArray(moviesArray)) {
-                throw new Error('Response does not contain an array of movies');
-            }
-    
-            return moviesArray;
-        } catch (error) {
-            console.error('Error fetching movies:', error);
-            throw error;
-        }
+async function fetchMovies(genre) {
+  const url = 'https://moviesverse1.p.rapidapi.com/get-by-genre';
+  const apiKey = 'b8d238b27fmsh972a2c298f17612p12fafejsn325b02608b36'; 
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+      'X-RapidAPI-Host': 'moviesverse1.p.rapidapi.com',
+    },
+  };
+
+  try {
+    const response = await fetch(url + `?genre=${genre}`, options);
+    const data = await response.json();
+
+    // Assuming the API response contains a key like 'movies' that holds the array
+    const moviesArray = data.movies || [];
+    console.log('Fetched movies:', moviesArray);
+
+    if (!Array.isArray(moviesArray)) {
+      throw new Error('Response does not contain an array of movies');
     }
-    
+
+    return moviesArray;
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+    throw error;
+  }
+}
